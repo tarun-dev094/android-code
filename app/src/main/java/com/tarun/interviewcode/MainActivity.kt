14376801dcd76876d722a3860237f54c1c625a6e
tarun-dev094 <59257094+tarun-dev094@users.noreply.github.com>
2020-04-10 23:38:56 +0530

    Hello
    Working on project.

--- app/src/main/java/com/tarun/interviewcode/MainActivity.kt
+++ app/src/main/java/com/tarun/interviewcode/MainActivity.kt
@@ -2,11 +2,20 @@ package com.tarun.interviewcode
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    var sum=0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        sum=10+23
+        var button=findViewById<Button>(R.id.button)
+        button.setOnClickListener {
+            println("Check $sum")
+            Toast.makeText(this,"Hello Welcome to this project",Toast.LENGTH_LONG).show()
+        }
     }
 }
